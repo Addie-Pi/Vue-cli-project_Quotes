df79c86560775cdbf4cd9bda87a412ffd7b4e04c
Addie-Pi <cureantidote@hotmail.com>
2020-10-14 03:32:36 +0800

    moving files

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="container">
+    <app-Header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-Header>
+    <app-New-Quote @quoteAdded="newQuote"></app-New-Quote>
+    <app-Quote-Grid :quotes="quotes" @quoteDeleted="quoteDeleted"></app-Quote-Grid>
+    <div class="row">
+      <div class="col-sm-12 text-center">
+        <div class="alert alert-info">Info: Click on a Quote to delete it!</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import QuoteGrid from "./components/QuoteGrid.vue";
+import NewQuote from "./components/NewQuote.vue";
+import Header from "./components/Header.vue";
+
+export default {
+  data: function () {
+    return {
+      quotes: ["Just a Quote to see something"],
+      maxQuotes: 10,
+    };
+  },
+  methods: {
+    newQuote(quoteToAdd) {
+      if (this.quotes.length >= this.maxQuotes) {
+        return alert("Please delete Quote first");
+      }
+      this.quotes.push(quoteToAdd);
+    },
+    quoteDeleted(index) {
+      this.quotes.splice(index, 1);
+    },
+  },
+  components: {
+    "app-Quote-Grid": QuoteGrid,
+    "app-New-Quote": NewQuote,
+    "app-Header": Header,
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/QuoteGrid.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="row">
+    <app-Quote
+      v-for="(quote, index) in quotes"
+      :key="quote"
+      @click.native="deleteQuote(index)"
+    >{{quote}}</app-Quote>
+    <!-- Vue 2.2.0+的版本里，当在组件中使用v-for时，key是必须的 -->
+  </div>
+</template>
+
+<script>
+import Quote from "./Quote.vue";
+export default {
+  props: ["quotes"],
+
+  components: {
+    "app-Quote": Quote,
+  },
+  methods: {
+    deleteQuote(index) {
+      this.$emit("quoteDeleted", index);
+    },
+  },
+};
+</script>
